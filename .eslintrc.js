b1472145b5b8a7373f6b5cc8c756c14b20f46714
Dohu <>
2022-06-26 17:42:24 +0900

    ESLInt Setting & Add TodoInput

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,27 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
-  ],
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/typescript/recommended', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true, // 따옴표는 홑따옴표를 사용한다. (jsx는 이 설정을 무시한다.)
+        semi: true, // 모든 구문에 세미콜론을 붙인다.
+        useTabs: false, // Tab을 사용 안 한다.
+        tabWidth: 2, // 들여쓰기 할 때, 기본 폭을 설정한다.
+        trailingComma: 'all', // 객체, 배열 등에서 항상 마지막에 , 를 붙인다.
+        printWidth: 200, // 한 줄에서 wrap 이 되는 기준의 글자 수를 정한다.
+        bracketSpacing: true, // 객체 리터럴의 괄호 사이에 공백을 출력
+        arrowParens: 'avoid', // 화살표 함수에서 단일 파라미터에 괄호를 붙일지("always") 말지("avoid")에 대한 여부를 결정한다.
+        bracketSameLine: true, // HTML Tag의 위치 여부
+        htmlWhitespaceSensitivity: 'ignore', // HTML tag 사이 공백 처리 여부
+      },
+    ],
   },
 };
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <label for="todo-input"></label>
+    <input type="text" name="" id="todo-input" :value="item" @input="handleInput" />
+    <button @click="addTodo" type="button">add</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  props: {
+    item: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    addTodo() {
+      this.$emit('add');
+    },
+    handleInput(event: InputEvent) {
+      const eventTarget = event.target as HTMLInputElement;
+      this.$emit('input', eventTarget.value);
+    },
+  },
+});
+</script>
+
+<style></style>
